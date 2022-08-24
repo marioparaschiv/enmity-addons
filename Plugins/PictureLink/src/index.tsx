ba63ad0cf679ef3eac797b2bb6cc3f2131243a5b
eternal <eternal.git@gmail.com>
2022-08-24 13:54:46 +0100

    fix picture link

--- Plugins/PictureLink/src/index.tsx
+++ Plugins/PictureLink/src/index.tsx
@@ -11,14 +11,14 @@ const [
    Header,
    Router
 ] = bulk(
-   m => m.default?.name === 'ProfileBanner',
-   m => m.default?.name === 'HeaderAvatar',
+   filters.byName('ProfileBanner', false),
+   filters.byName('HeaderAvatar', false),
    filters.byProps('transitionToGuild')
 );
 
 const PictureLink: Plugin = {
    name: 'PictureLink',
-   version: '1.0.0',
+   version: '1.0.1',
    description: "Allows you to click avatars and banners to open them in-app.",
    authors: [
       {
@@ -29,17 +29,20 @@ const PictureLink: Plugin = {
 
    onStart() {
       Patcher.after(Header, 'default', (_, [{ user }], res) => {
-         const image = user?.getAvatarURL?.(false, 4096, true)?.replace('.webp', '.png');
-
+         const image = user?.getAvatarURL?.(false, 4096, true);
          if (!image) return res;
 
-         return <Pressable onPress={() => Router.openURL(image)}>
+         const discrim = user.discriminator % 5;
+         const url = typeof image === 'number' ? `https://cdn.discordapp.com/embed/avatars/${discrim}.png` : image?.replace('.webp', '.png');
+
+         return <Pressable onPress={() => Router.openURL(url)}>
             {res}
          </Pressable>;
       });
 
       Patcher.after(ProfileBanner, 'default', (_, [{ bannerSource }], res) => {
-         if (!bannerSource?.uri) return res;
+         if (typeof bannerSource?.uri !== 'string' || !res) return res;
+
          const image = bannerSource.uri
             .replace(/(?:\?size=\d{3,4})?$/, '?size=4096')
             .replace('.webp', '.png');
