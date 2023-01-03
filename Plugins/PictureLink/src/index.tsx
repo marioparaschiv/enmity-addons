caafd667a0315ca8beb3c2e2a14ef9eac8a4c631
eternal <eternal.git@gmail.com>
2023-01-03 09:25:42 +0000

    fix picture link

--- Plugins/PictureLink/src/index.tsx
+++ Plugins/PictureLink/src/index.tsx
@@ -1,4 +1,5 @@
 import { Plugin, registerPlugin } from 'enmity/managers/plugins';
+import { wrapInHooks } from 'enmity/utilities';
 import { Pressable } from 'enmity/components';
 import { bulk, filters } from 'enmity/metro';
 import { React } from 'enmity/metro/common';
@@ -7,55 +8,60 @@ import { create } from 'enmity/patcher';
 const Patcher = create('picture-link');
 
 const [
-   ProfileBanner,
-   Header,
-   Router
+  ProfileBanner,
+  Header,
+  Router
 ] = bulk(
-   filters.byName('ProfileBanner', false),
-   filters.byName('HeaderAvatar', false),
-   filters.byProps('transitionToGuild')
+  filters.byName('ProfileBanner', false),
+  filters.byName('HeaderAvatar', false),
+  filters.byProps('transitionToGuild')
 );
 
 const PictureLink: Plugin = {
-   name: 'PictureLink',
-   version: '1.0.1',
-   description: "Allows you to click avatars and banners to open them in-app.",
-   authors: [
-      {
-         name: 'eternal',
-         id: '263689920210534400'
-      }
-   ],
+  name: 'PictureLink',
+  version: '1.0.1',
+  description: "Allows you to click avatars and banners to open them in-app.",
+  authors: [
+    {
+      name: 'eternal',
+      id: '263689920210534400'
+    }
+  ],
 
-   onStart() {
-      Patcher.after(Header, 'default', (_, [{ user }], res) => {
-         const image = user?.getAvatarURL?.(false, 4096, true);
-         if (!image) return res;
+  onStart() {
+    Patcher.after(Header, 'default', (_, [{ user, style }], res) => {
+      const image = user?.getAvatarURL?.(false, 4096, true);
+      if (!image) return res;
 
-         const discrim = user.discriminator % 5;
-         const url = typeof image === 'number' ? `https://cdn.discordapp.com/embed/avatars/${discrim}.png` : image?.replace('.webp', '.png');
+      const discrim = user.discriminator % 5;
+      const url = typeof image === 'number' ? `https://cdn.discordapp.com/embed/avatars/${discrim}.png` : image?.replace('.webp', '.png');
 
-         return <Pressable onPress={() => Router.openURL(url)}>
-            {res}
-         </Pressable>;
-      });
+      delete res.props.style;
 
-      Patcher.after(ProfileBanner, 'default', (_, [{ bannerSource }], res) => {
-         if (typeof bannerSource?.uri !== 'string' || !res) return res;
+      return <Pressable
+        onPress={() => Router.openURL(url)}
+        style={style}
+      >
+        {res}
+      </Pressable>;
+    });
 
-         const image = bannerSource.uri
-            .replace(/(?:\?size=\d{3,4})?$/, '?size=4096')
-            .replace('.webp', '.png');
+    Patcher.after(ProfileBanner, 'default', (_, [{ bannerSource }], res) => {
+      if (typeof bannerSource?.uri !== 'string' || !res) return res;
 
-         return <Pressable onPress={() => Router.openURL(image)}>
-            {res}
-         </Pressable>;
-      });
-   },
+      const image = bannerSource.uri
+        .replace(/(?:\?size=\d{3,4})?$/, '?size=4096')
+        .replace('.webp', '.png');
 
-   onStop() {
-      Patcher.unpatchAll();
-   },
+      return <Pressable onPress={() => Router.openURL(image)}>
+        {res}
+      </Pressable>;
+    });
+  },
+
+  onStop() {
+    Patcher.unpatchAll();
+  },
 };
 
 registerPlugin(PictureLink);
